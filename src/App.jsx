3d272562a2488f3db2401f94be99910fb30b5326
Jay panchal <jaypanchal06334@gmail.com>
2025-04-26 12:54:52 +0530

    app create

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-amber-500">hello</div>
+    </>
+  );
+}
+
+export default App;
